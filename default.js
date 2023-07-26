d43f42c2fcb4f55856bbf5686bac6805812cbc17
Md.Abdulla Al Riad <70167682+Riad889@users.noreply.github.com>
2023-07-26 22:14:52 +0600

    Update default.js

--- default.js
+++ default.js
@@ -1,13 +1,1 @@
-const product=require('../server/constrains/data');
-const ProductDetail=require('../server/models/product-schema');
-const defaultData = async () => {
-    try {
-       // console.log(product);
-       // await ProductDetail.insertMany(product);
-        //console.log("Data inserted successfully");
-    } catch (error) {
-        console.log(error.message);
-        
-    }
-};
-module.exports = defaultData;
+
