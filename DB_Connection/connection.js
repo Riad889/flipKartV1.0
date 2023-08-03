92ffe076fbcd2c52b4186faa22034024608c6cea
Md.Abdulla Al Riad <70167682+Riad889@users.noreply.github.com>
2023-08-03 13:10:55 +0600

    update connection.js

--- DB_Connection/connection.js
+++ DB_Connection/connection.js
@@ -1,13 +1,13 @@
 const mongoose=require('mongoose');
 
 //Db connection url
-const DB_URL=process.env.DB_URL;
+const DB_URL1=process.env.DB_URL;
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverSelectionTimeoutMS: 30000, // Increase the timeout value to 30 seconds
   };
- mongoose.connect(DB_URL,options).then(()=>{
+ mongoose.connect(DB_URL1,options).then(()=>{
     console.log("Database connected");
 }).catch((error)=>{
     console.log(error);
